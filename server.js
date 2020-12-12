d8bb037b2cf604c54da15906ba6053d6dd074c34
Girma Ebssa <girma.ebssa@gmail.com>
2020-12-12 12:32:40 -0800

    adds code to server.js and include express-handlebars are dep

---
+++ server.js
@@ -0,0 +1,22 @@
+// Requiring necessary npm packages
+const express = require("express");
+
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
+});
