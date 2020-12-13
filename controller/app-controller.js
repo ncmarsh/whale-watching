9be24bc531139905a0e89163ebf47e47422aab67
Dan Cornutt <dacornutt@gmail.com>
2020-12-13 12:57:25 -0800

    Added controller functionality

--- controller/app-controller.js
+++ controller/app-controller.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Import the models
 const db = require("../models");
 
@@ -27,29 +26,6 @@ module.exports = function(app) {
       };
       res.render("sightings", hbsObject);
     });
-=======
-// const express = require("express");
-// const router = express.Router();
-
-// // Import the models
-const db = require("../models");
-
-module.exports = function(app) {
-  app.get("/", function(req, res) {
-    // db.Sighting.findAll(function(data) {
-    //   let hbsObject = {
-    //     sightings: data
-    //   };
-    //   console.log(hbsObject);
-    //   res.render("index", hbsObject);
-    // });
-    db.Sighting.findAll({})
-      .then(function(data) {
-        let hbsObject = {
-          sightings: data
-        };
-      });
->>>>>>> Develop
   });
 
 }
