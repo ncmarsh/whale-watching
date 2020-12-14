3b16a32f270c16c9bbba496a002f87d43061a868
Girma Ebssa <girma.ebssa@gmail.com>
2020-12-14 12:48:58 -0800

    include passport-local authentication and checkIfLoggedIn constraints

---
+++ config/passport.js
@@ -0,0 +1,42 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const db = require("../models");
+
+// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      // When a user tries to sign in this code runs and ckecks if a user exists with this username
+        db.User.findOne({  
+           where: {
+            username: username      
+           }                    
+        })
+        .then(user => {
+        //If a user is not found with the entered username
+        if (!user) {
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+        //if password is not valid
+        if (!user.validPassword(password)) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+        //If username and password are correct, return the user
+        return done(null, user);
+        })        
+    }
+));
+
+// In order to help keep authentication state across HTTP requests,
+// Sequelize needs to serialize and deserialize the user
+// Just consider this part boilerplate needed to make it all work
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+// Exporting our configured passport
+module.exports = passport;
