dd26a905c57077c6b1f9c6b32a6da9b62a278cc2
Olga Illarionova <ola.illari@gmail.com>
2020-12-14 18:23:22 -0800

    reload page after logging in

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
     // Getting references to our form and inputs
     var loginForm = $("form.login");
-    var userNameInput = $("input#userName-input");
+    var userNameInput = $("input#username-input");
     var passwordInput = $("input#password-input");
   
     // When the form is submitted, we validate there's an email and password entered
@@ -25,17 +25,27 @@ $(document).ready(function() {
   
     // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
     function loginUser(userName, password) {
+        //console.log(userName,password,'api/login');
       $.post("/api/login", {
         userName: userName,
         password: password
       })
         .then(function() {
-          window.location.replace("/members");  ///Change this
+          window.location.reload();  ///Change this
           // If there's an error, log the error
         })
         .catch(function(err) {
           console.log(err);
         });
     }
+    //test function DELETE
+    /* function loginUser2(userName, password) {
+        console.log(userName,password,'api/login');
+      
+        
+          window.location.reload()  ///Change this
+          // If there's an error, log the error   
+    } */
+
   });
   
