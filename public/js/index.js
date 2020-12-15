90d4340a0b90df1b6671684f9873b5fe6d76fdc5
Olga Illarionova <ola.illari@gmail.com>
2020-12-14 18:23:59 -0800

    some work about new posts

--- public/js/index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $("#select-city").on("click", function(event) {
-        city = $("#city").val().trim();
-        window.location.replace("/city/"+city);
-      });
-})
---
+++ public/js/posts.js
@@ -0,0 +1,56 @@
+const sighting = require("../../models/sighting");
+
+$(document).ready(function() {
+    $("#select-city").on("click", function(event) {
+        let city = $("#city").val().trim();
+        window.location.replace("/city/"+city);
+    });
+
+    /**
+     * CREATE NEW POST
+     * Click button create-post
+     * Check if user not logged in: ////How to do this
+     *      give alert that you mast login or signup
+     *      else // if looged
+     *          collect user data (id or username)
+     *          show modal with form 
+     *          on submit button call POST (/api/sightings)
+     *          then go to /sightings
+     */
+    $("#create-post").on("click",function(event){
+        $.get("/api/user_data").then(function(data) {
+            if (!data){
+                ///show instead of form error message
+            }
+            else{
+                let userId = data.id   
+                let city = $("new-city").val();
+                let specificLocation = $("#specificLocation").val().trim();
+                let description = $("#description").val().trim();
+                ///data from form maybe changed depend of form
+                new_sighting = {
+                    UserID: userId,
+                    city: city,
+                    specificLocation: specificLocation,
+                    description: description
+                }
+                $.post('/api/sightings',new_sighting).then(function(data){
+                    window.location.reload();
+                }) 
+            }
+            
+          });
+    })
+ //// Example of conditional modal   
+ /* 
+    $("#my-test-btn").on("click",function(event){
+        let data = 0; ///=1
+        if (data===0){
+            errMsg = `<h2>You should be logged to create new posts</h2>`;
+            $("#my-test-modal").html(errMsg)
+        }
+    })
+*/
+
+    ///Change to post request
+})
