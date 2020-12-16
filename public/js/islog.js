be4263b2e1e221e27caa1faf12324b087a578712
Olga Illarionova <ola.illari@gmail.com>
2020-12-16 10:47:15 -0800

    change in boilerplate islogged

--- public/js/islog.js
+++ public/js/islog.js
@@ -14,16 +14,24 @@ $(document).ready(function() {
      * }
      * })
      */
-    $,get("api/user-data").then(function(data){
+    $.get("api/user_data").then(function(data){
         if(!data){
             /// show login sign up
+            $("#login-link").show();
+            $("#signup-link").show();
+            $("#logout-link").hide();
+            $("#acc-link").hide()
         }
         else{
             ///hide login signup 
+            $("#login-link").hide();
+            $("#signup-link").hide();
             // show logout 
+            $("#logout-link").show();
+            $("#acc-link").show()
             // and name
             let fullName = data.firstName + ' '+data.lastName;
-            
+            console.log(fullName);
         }
     })
 });
