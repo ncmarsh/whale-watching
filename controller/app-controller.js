532ddeb4b2e674b6cbcf72a9fb8c3b8a92d2b6a5
Dan Cornutt <dacornutt@gmail.com>
2020-12-15 20:32:50 -0800

    Formatting controller to pretty data

--- controller/app-controller.js
+++ controller/app-controller.js
@@ -21,6 +21,11 @@ module.exports = function(app) {
   //Serve home handlebars page & required data
   app.get("/sightings", function(req, res) {
     db.Sighting.findAll({}).then(function(data) {
+      // createdAt
+      // city
+      // description
+      // user
+      console.log(data);
       let hbsObject = {
         data: data 
       };
